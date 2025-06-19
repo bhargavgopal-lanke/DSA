b89cb521315fd84c6d071bf4263d846989124a88
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-19 16:21:17 -0400

    refactor: update array initialization and improve function logic for searching and finding numbers

--- Examples/example.js
+++ Examples/example.js
@@ -1,7 +1,7 @@
 //   write a function that searches for an element in an array
 //  and return the index , and of the element is not present return -1
 
-const arr = [4, 2, 0, 10, 8, 30];
+const arr = [-1, 4, 2, 0, 10, 8, 30, -2, -11];
 
 function searchElement(arr, n) {
   for (let i = 0; i < arr.length; i++) {
@@ -21,7 +21,7 @@ function negativeNUmbers(arr) {
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < 0) {
-      console.log("negative numbers", arr[i]);
+      //   console.log("negative numbers", arr[i]);
       count++;
     }
   }
@@ -29,12 +29,12 @@ function negativeNUmbers(arr) {
 }
 
 const resNegative = negativeNUmbers(negativeArr);
-console.log("resNegative", resNegative);
+// console.log("resNegative", resNegative);
 
 // write a function that returns the largest number in an array
-
+const maxArr = [-9, -11, -3, 1, 0, 11];
 function findLargetNumber(arr) {
-  let num = -1;
+  let num = maxArr[0];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > num) {
       num = arr[i];
@@ -43,5 +43,20 @@ function findLargetNumber(arr) {
   return num;
 }
 
-let resLargest = findLargetNumber(arr);
+let resLargest = findLargetNumber(maxArr);
 console.log("resLargest", resLargest);
+
+// find the minimum number from an array
+
+function findMinNumber(arr) {
+  let minNum = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < minNum) {
+      minNum = arr[i];
+    }
+  }
+  return minNum;
+}
+
+let resMin = findMinNumber(arr);
+console.log("resMin", resMin);

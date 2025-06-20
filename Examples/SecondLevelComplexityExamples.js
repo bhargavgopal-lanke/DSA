7a6da1f489a9f0ccde7849293cc07754758fe727
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-20 15:33:44 -0400

    feat: add HTML and JavaScript files for star patterns example

--- Examples/SecondLevelComplexityExamples.js
+++ Examples/SecondLevelComplexityExamples.js
@@ -1,8 +1,8 @@
 const secArr = [14, 14, 12, 4, 9, 0, 2, 8, 7, 1];
 
 function findSecondLargestNumber(arr) {
-  let firstLargest = -Infinity; // 14,  
-  let secondLargest = -Infinity; // -inf, 12, 
+  let firstLargest = -Infinity; // 14,
+  let secondLargest = -Infinity; // -inf, 12,
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > firstLargest) {
       secondLargest = firstLargest;
@@ -19,4 +19,37 @@ function findSecondLargestNumber(arr) {
 }
 
 const secRes = findSecondLargestNumber(secArr);
-console.log("secRes", secRes);
+// console.log("secRes", secRes);
+
+// remove the duplicates from the array
+
+function removeDuplicates(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== secArr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+const dupeRes = removeDuplicates(secArr);
+// console.log("dupeRes", dupeRes);
+
+// for (let i = 0; i < 3; i++) {
+//   for (let j = i; j >= 0; j--) {
+//     console.log("i", i + "  " + "j", j);
+//   }
+// }
+
+// for (let i = 5; i > 0; i--) {
+//   for (let j = 0; j < i; j++) {
+//     console.log("i", i + "  " + "j", j);
+//   }
+// }
+
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < 4; j++) {
+    console.log(i, j);
+  }
+}

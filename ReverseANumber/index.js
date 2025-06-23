efbfacebe05375705e7413d3838a45384eaa7cd5
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-23 17:27:50 -0400

    fix: handle negative numbers in reverseNum function

--- ReverseANumber/index.js
+++ ReverseANumber/index.js
@@ -1,13 +1,14 @@
 function reverseNum(n) {
-  if (n < 0) return;
+  let copyN = n;
   let rev = 0;
+  n = Math.abs(n);
   while (n > 0) {
     let rem = n % 10; // gives the remainder 8,
     rev = rev * 10 + rem; // 0 + 8 = 8, 80 +
     n = Math.floor(n / 10); // removes last number and gives remaining numbers 3777, condition runs until this number becomes 0
   }
-  return rev;
+  return copyN < 0 ? -rev : rev;
 }
 
-const result = reverseNum(37778);
+const result = reverseNum(-37778);
 console.log("result", result);

117a9febe5c5075a5154b2ab8539c331c8393bbc
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-25 17:25:10 -0400

    fix: add overflow check for reversed number in reverseNum function

--- ReverseANumber/index.js
+++ ReverseANumber/index.js
@@ -7,6 +7,8 @@ function reverseNum(n) {
     rev = rev * 10 + rem; // 0 + 8 = 8, 80 +
     n = Math.floor(n / 10); // removes last number and gives remaining numbers 3777, condition runs until this number becomes 0
   }
+  let limit = Math.pow(2, 31);
+  if (rev < -limit || rev > limit) return 0;
   return copyN < 0 ? -rev : rev;
 }
 

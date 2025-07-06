dc8b91b3c8f8758a9ca22abdb38a2230a9f512e8
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-07-06 11:38:43 +0530

    feat: add function to check if a number is a palindrome

--- ReverseString/reverseInteger.js
+++ ReverseString/reverseInteger.js
@@ -19,3 +19,24 @@ function reverseInt2(num) {
 
 let intres1 = reverseInt2(1234);
 console.log("intres1", intres1);
+
+// check a number is a palindrome or not
+
+function palindrome(n) {
+  let ncopy = Math.abs(n);
+  let reverse = 0;
+
+  while (ncopy > 0) {
+    let rem = ncopy % 10;
+    reverse = reverse * 10 + rem;
+    ncopy = Math.floor(ncopy / 10);
+  }
+  if (reverse === n) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let palindromeres = palindrome(121);
+console.log("palindromeres", palindromeres);

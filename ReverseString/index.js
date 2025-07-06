15d701090772a653f2ee12faee8b8ff82a5db670
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-07-06 10:14:42 +0530

    feat: add second implementation for reversing a string using concatenation

--- ReverseString/index.js
+++ ReverseString/index.js
@@ -12,3 +12,15 @@ function reverseString(s) {
 
 const res = reverseString(["g", "o", "p", "a", "l"]);
 console.log(res);
+
+function reverseString2(s) {
+  let reverse = "";
+  for (let i = 0; i < s.length; i++) {
+    reverse = s[i] + reverse;
+  }
+  return reverse;
+}
+
+let strReverse = reverseString2("bhargav");
+
+console.log(strReverse);

5c12e19c45772095a2ba2f9e8a088b4d106ee12c
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-07-06 10:18:33 +0530

    feat: add third implementation for reversing a string using a for-of loop

--- ReverseString/index.js
+++ ReverseString/index.js
@@ -24,3 +24,15 @@ function reverseString2(s) {
 let strReverse = reverseString2("bhargav");
 
 console.log(strReverse);
+
+function reverseString3(str) {
+  let reverse = "";
+  // simple way to write a for loop. clean and clear
+  for (let char of str) {
+    reverse = char + reverse;
+  }
+  return reverse;
+}
+
+let res3 = reverseString3("Gopal");
+console.log("res3", res3);

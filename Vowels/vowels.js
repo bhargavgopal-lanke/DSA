a73b967ae70b08b3de06ff892246d2b09fbf72d7
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-07-08 12:40:32 +0530

    feat: add additional functions for counting vowels in a string

--- Vowels/vowels.js
+++ Vowels/vowels.js
@@ -26,3 +26,27 @@ function findVowels(str) {
 }
 
 console.log(findVowels(str));
+
+// simple example to find number of vowels insdie a sentence
+
+function noofVowels(str) {
+  let regExp = str.match(/[aeiou]/gi);
+  return regExp ? RegExp.length : 0;
+}
+
+console.log(noofVowels("this i a setnence"));
+
+// other way to find vowels inside a sentence
+
+function vowelsArrMethod(str) {
+  let vowelArr = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vowelArr.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(vowelsArrMethod("this is a new example to test vowels"));
